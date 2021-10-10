1220bfa73e61c46bb892a4e9a51193def17f8ea1
Pragyan Khati <83399260+rampdedlizer@users.noreply.github.com>
2021-10-10 15:31:27 +0545

    Two Sum Solutions

--- STL.cpp
+++ STL.cpp
@@ -130,3 +130,18 @@ int main()
 {
   std::cout<<encrypt("32104",1);
 }
+
+
+//Two sum solutions
+std::pair<std::size_t,std::size_t> two_sum(std::vector<int> numbers, int target)
+{
+  for(int i=0;i<numbers.size();i++)
+  {
+    for(int j=(i+1);j<numbers.size();j++)
+    {
+      if(numbers[i]+numbers[j]==target)
+        return{i,j};
+    }
+  }   
+  return{0,0};
+}
